89ad6c9e4c717b76091208d518f295d3e743b9a5
Ryan Barclay <mrryanbarclay@gmail.com>
2023-11-21 08:22:21 -0800

    52 done

---
+++ 3-Hard/52. N-Queens II/totalNQueens.ts
@@ -0,0 +1,56 @@
+function totalNQueens(n: number): number {
+  let board: string[][] = Array(n)
+    .fill(".")
+    .map(() => Array(n).fill(".")); // n x n board
+
+  // init helper functions
+  const result: string[][] = [];
+
+  const checkDiagonal = (row: number, col: number, board: string[][]) => {
+    for (let x = row, y = col; x >= 0 && y >= 0; x--, y--) {
+      if (board[x][y] === "Q") return false;
+    } // check upper left diagonal
+
+    for (let x = row, y = col; x < board.length && y >= 0; x++, y--) {
+      if (board[x][y] === "Q") return false;
+    } // check lower left diagonal
+
+    for (let x = row, y = col; x >= 0 && y < board.length; x--, y++) {
+      if (board[x][y] === "Q") return false;
+    } // check upper right diagonal
+
+    for (let x = row, y = col; x < board.length && y < board.length; x++, y++) {
+      if (board[x][y] === "Q") return false;
+    } // check lower right diagonal
+
+    return true;
+  };
+
+  const isValid = (row: number, col: number, board: string[][]) => {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][col] === "Q") return false; // check col
+      if (board[row][i] === "Q") return false; // check row
+    }
+
+    return checkDiagonal(row, col, board);
+  };
+
+  const backtrack = (row: number, board: string[][]) => {
+    if (row === board.length) {
+      // base case: all queens possible placed has been explored
+      result.push(board.map((row) => row.join("")));
+      return;
+    }
+
+    for (let col = 0; col < board.length; col++) {
+      if (isValid(row, col, board)) {
+        board[row][col] = "Q"; // place queen
+        backtrack(row + 1, board); // explore options with queen placed
+        board[row][col] = "."; // remove queen
+      }
+    }
+  };
+
+  backtrack(0, board);
+  return result.length;
+}
